fe5d4b0dd2a9e827518ccedc9a203f276473b83e
Norbert Goździk <norbert1303@wp.pl>
2022-02-23 20:20:33 +0100

    update menu-items to navigate with react-router-dom 6

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = (props) => {
+    console.log(props)
+    return (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)}
+const HatsPageId = () => (
+    <div>
+        <h1>HATS PAGE IDDDDD</h1>
+    </div>
+)
+
 function App() {
   return (
     <div>
-     <HomePage/>
+        <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/hats" element={<HatsPage/>}/>
+            <Route path="/hats/:hatsId" element={<HatsPageId/>}/>
+        </Routes>
     </div>
   );
 }
