51f4040d5723ffd854bfac022bd6f0e8b6b66ca3
Norbert Goździk <norbert1303@wp.pl>
2022-06-01 21:42:17 +0200

    Adding cart context and shopping icon

--- crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useContext, useState} from 'react';
 import {Button} from '../button/button.component';
 import {
     createAuthUserWithEmailAndPassword,
@@ -6,6 +6,7 @@ import {
 } from '../../utils/firebase/firebase.utils';
 import './sign-up-form.styles.scss';
 import {FormInput} from "../form-input/form.input.component";
+import {UserContext} from "../../contexts/user.context";
 
 const defaultFormFields = {
     displayName: '',
@@ -17,6 +18,8 @@ const defaultFormFields = {
 export const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
+
+    // const {setCurrentUser} =useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -35,6 +38,8 @@ export const SignUpForm = () => {
                 email,
                 password
             );
+
+            // setCurrentUser(user);
 
             await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {UserProvider} from "./contexts/user.context";
+import {ProductsProvider} from "./contexts/products.context";
+import {CartProvider} from "./contexts/cart.context";
 
 
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
-        <App />
+          <UserProvider>
+              <ProductsProvider>
+                  <CartProvider>
+                    <App />
+                  </CartProvider>
+              </ProductsProvider>
+          </UserProvider>
       </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
