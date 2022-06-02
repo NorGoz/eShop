1d5f3ee4cf398fdf3da6d4da056970d85969fedb
Norbert Goździk <norbert1303@wp.pl>
2022-06-02 22:08:20 +0200

    Adding cart-item,cart-down

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,18 @@
+import {useContext} from "react";
+
+import {CartContext} from "../../contexts/cart.context";
 import './cart-dropdown.style.scss';
 import {Button} from "../button/button.component";
+import {CartItem} from "../cart-item/cart-item.component";
 
 export const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext)
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
             </div>
+                <Button>GO TO CHECKOUT</Button>
         </div>
     )
 }
